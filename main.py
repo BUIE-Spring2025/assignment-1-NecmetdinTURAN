c2a7f5d8f5d98c38d02861305b5c13c988455421
NecmetdinTURAN <necmetdin.turan@std.bogazici.edu.tr>
2025-03-02 18:49:52 +0300

    First Step

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ def int_to_roman(num):
     :param num: Integer value between 1 and 3999 inclusive.
     :return: A string representing the Roman numeral of the integer.
     """
+    lengthofnum = 0
+    if num <= 9:
+        lengthofnum=1
+    elif num<= 99:
+        lengthofnum=2
+    elif num <= 999:
+        lengthofnum=3
+    elif num <= 3999:
+        lengthofnum=4
+    
