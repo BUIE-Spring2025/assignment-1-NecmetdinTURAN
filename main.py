0663d22ed2a73780c5811cbf4711ab9afe33663a
NecmetdinTURAN <necmetdin.turan@std.bogazici.edu.tr>
2025-03-02 19:11:27 +0300

    Halfway done

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ def int_to_roman(num):
     :param num: Integer value between 1 and 3999 inclusive.
     :return: A string representing the Roman numeral of the integer.
     """
-    lengthofnum = 0
     outcomestring=""
     if num <= 9:
         lengthofnum=1
@@ -16,15 +15,22 @@ def int_to_roman(num):
     elif num <= 3999:
         lengthofnum=4
     # 4 and 9 are special occasions otherwise numbers can be stacked side by side
-    while lengthofnum==4:
-        if 1000<=num<=3999:
-            outcomestring += "M"
-            num -= 1000
-            if num <= 9:
-                lengthofnum=1
-            elif num<= 99:
-                lengthofnum=2
-            elif num <= 999:
-                lengthofnum=3
-            elif num <= 3999:
-                lengthofnum=4
+    while 1000<=num<=3999:
+        outcomestring += "M"
+        num -= 1000
+    if 900 <= num <= 999:
+        outcomestring += "CM"
+        num -= 900
+    elif  600<= num <= 899:
+        outcomestring += "D"
+        num -= 500
+    elif 400<=num<=499:
+        outcomestring +="CD"
+        num -= 400
+    while 100<= num <= 399:
+        outcomestring +="C"
+        num -= 100
+    
+
+    
+
