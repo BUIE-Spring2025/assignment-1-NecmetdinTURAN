43f136c075396d1665ef5bb5811148bd875ad558
NecmetdinTURAN <necmetdin.turan@std.bogazici.edu.tr>
2025-03-02 19:01:30 +0300

    Main idea

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def int_to_roman(num):
     :return: A string representing the Roman numeral of the integer.
     """
     lengthofnum = 0
+    outcomestring=""
     if num <= 9:
         lengthofnum=1
     elif num<= 99:
@@ -14,4 +15,16 @@ def int_to_roman(num):
         lengthofnum=3
     elif num <= 3999:
         lengthofnum=4
-    
+    # 4 and 9 are special occasions otherwise numbers can be stacked side by side
+    while lengthofnum==4:
+        if 1000<=num<=3999:
+            outcomestring += "M"
+            num -= 1000
+            if num <= 9:
+                lengthofnum=1
+            elif num<= 99:
+                lengthofnum=2
+            elif num <= 999:
+                lengthofnum=3
+            elif num <= 3999:
+                lengthofnum=4
